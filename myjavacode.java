7d3986c841664d394a5fcf8e3656a3b2f4898273
Priyam123456 <priyambhu111@gmail.com>
2024-06-22 22:38:02 +0530

    myjava

--- myjavacode.java
+++ myjavacode.java
@@ -5,4 +5,14 @@ my fisrt line code
 this is my first line code.
 this is my 2nd line code 
 this is my 3rd line code
+<<<<<<< Updated upstream
 my mangaer task completed 
+=======
+
+
+
+this is my 4th line code
+this is my 5th line code
+this is my 6th line code
+
+>>>>>>> Stashed changes
